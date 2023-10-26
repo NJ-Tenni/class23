de7f807f3097e4daeeef0cdc24962e8bf5b3cc5e
NJ-Tenni <143335754+NJ-Tenni@users.noreply.github.com>
2023-10-26 20:11:45 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,12 @@
+let num = Number(prompt());
+
+function number(a, b) {
+    if (num % 2 == 0) {
+        document.write("The number is Even");
+    }
+    else{
+        document.write("The number is Odd");
+    }
+}
+
+number();
